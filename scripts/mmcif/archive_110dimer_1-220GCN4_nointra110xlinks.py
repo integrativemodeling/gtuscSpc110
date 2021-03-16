ed5532990a860b1bb7ae7c86fe2a2bf55e2d1fdb
Ben Webb <ben@salilab.org>
2021-03-15 17:27:07 -0700

    Use modern APIs

--- scripts/mmcif/archive_110dimer_1-220GCN4_nointra110xlinks.py
+++ scripts/mmcif/archive_110dimer_1-220GCN4_nointra110xlinks.py
@@ -88,7 +88,7 @@ mdl = IMP.Model()
 s = IMP.pmi.topology.System(mdl)
 
 # Add deposition information
-po = IMP.pmi.mmcif.ProtocolOutput(None)
+po = IMP.pmi.mmcif.ProtocolOutput()
 s.add_protocol_output(po)
 po.system.title = "Integrative structure of the yeast gammaTuSC-Spc110 dimer complex"
 # po.system.citations.append(ihm.Citation.from_pubmed_id(000000)) #TODO
@@ -204,7 +204,7 @@ kw_edc.set_residue2_key("POSITION2")
 xldb_edc = IMP.pmi.io.crosslink.CrossLinkDataBase(kw_edc)
 xldb_edc.create_set_from_file(edc_file)
 
-xlr_edc = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(root_hier=root_hier,CrossLinkDataBase=xldb_edc,length=18.0,label="XLEDC",filelabel='edc',resolution=1,slope=0.03)
+xlr_edc = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(root_hier=root_hier,database=xldb_edc,length=18.0,label="XLEDC",filelabel='edc',resolution=1,slope=0.03)
 
 xlr_edc.add_to_model()
 xlr_edc.set_weight(15.0)
@@ -221,7 +221,7 @@ kw_dss.set_residue2_key("POSITION2")
 xldb_dss = IMP.pmi.io.crosslink.CrossLinkDataBase(kw_dss)
 xldb_dss.create_set_from_file(dss_file)
 
-xlr_dss = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(root_hier=root_hier,CrossLinkDataBase=xldb_dss,length=28.0,label="XLDSS",filelabel='dss',resolution=1,slope=0.03)
+xlr_dss = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(root_hier=root_hier,database=xldb_dss,length=28.0,label="XLDSS",filelabel='dss',resolution=1,slope=0.03)
 
 xlr_dss.add_to_model()
 xlr_dss.set_weight(15.0)
