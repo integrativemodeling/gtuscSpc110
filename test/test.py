064c08188c4f29699e5e3dae2b9432e502101a90
Ben Webb <ben@salilab.org>
2021-03-17 15:39:18 -0700

    Fix assertions to pass with 1-core tests

--- test/test.py
+++ test/test.py
@@ -15,22 +15,24 @@ class Tests(unittest.TestCase):
     def test_gtusc_dimer(self):
         """Test dimer simulations"""
         os.chdir(os.path.join(TOPDIR, 'scripts/sample'))
+        if os.path.exists('output'):
+            shutil.rmtree('output')
         p = subprocess.check_call(["python", "sample_gtusc_110dimer_1-220GCN4_nointra110xlinks.py", "--test"])
 
         # require that the number of frames is present
         total_num_lines_stat_files = 0
 
-        for i in range(6):
+        for i in range(1):
             f = open("output/stat."+str(i)+".out")
 
             total_num_lines_stat_files += len(f.readlines())
 
             f.close()
 
-        self.assertEqual(total_num_lines_stat_files,1006)
+        self.assertEqual(total_num_lines_stat_files,10001)
 
         # require that output files were produced
-        for i in range(6):
+        for i in range(1):
             os.unlink("output/rmfs/"+str(i)+".rmf3")
 
             os.unlink("output/stat."+str(i)+".out")
@@ -42,22 +44,24 @@ class Tests(unittest.TestCase):
     def test_gtusc_monomer(self):
         """Test monomer simulations"""
         os.chdir(os.path.join(TOPDIR, 'scripts/sample'))
+        if os.path.exists('output'):
+            shutil.rmtree('output')
         p = subprocess.check_call(["python", "sample_gtusc_110mono_1-220GCN4_nointra110xlinks.py", "--test"])
 
         # require that the number of frames is present
         total_num_lines_stat_files = 0
 
-        for i in range(6):
+        for i in range(1):
             f = open("output/stat."+str(i)+".out")
 
             total_num_lines_stat_files += len(f.readlines())
 
             f.close()
 
-        self.assertEqual(total_num_lines_stat_files,1006)
+        self.assertEqual(total_num_lines_stat_files,10001)
 
         # require that output files were produced
-        for i in range(6):
+        for i in range(1):
             os.unlink("output/rmfs/"+str(i)+".rmf3")
 
             os.unlink("output/stat."+str(i)+".out")
@@ -67,22 +71,24 @@ class Tests(unittest.TestCase):
     def test_gtusc_tetramer(self):
         """Test tetramer simulations"""
         os.chdir(os.path.join(TOPDIR, 'scripts/sample'))
+        if os.path.exists('output'):
+            shutil.rmtree('output')
         p = subprocess.check_call(["python", "sample_gtusc_110tetramer_1-220GCN4_nointra110xlinks.py", "--test"])
 
         # require that the number of frames is present
         total_num_lines_stat_files = 0
 
-        for i in range(6):
+        for i in range(1):
             f = open("output/stat."+str(i)+".out")
 
             total_num_lines_stat_files += len(f.readlines())
 
             f.close()
 
-        self.assertEqual(total_num_lines_stat_files,1006)
+        self.assertEqual(total_num_lines_stat_files,20001)
 
         # require that output files were produced
-        for i in range(6):
+        for i in range(1):
             os.unlink("output/rmfs/"+str(i)+".rmf3")
 
             os.unlink("output/stat."+str(i)+".out")
